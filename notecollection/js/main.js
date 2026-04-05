282c69ce28468a73c5a17a77f08fe8b9a5d3be15
Tong-Xiangjie <2011746785@qq.com>
2026-04-05 22:50:47 +0800

    更新 main.js

--- notecollection/js/main.js
+++ notecollection/js/main.js
@@ -35,10 +35,6 @@ let currentY = 0;
 
 const KRAUSE_PREFIX = 'Pick# ';
 
-// 搜索结果页返回相关变量
-let fromSearchResult = false;
-let lastSearchParams = null;
-
 function saveScroll(key) {
     scrollMemory[key] = window.scrollY;
 }
@@ -189,7 +185,7 @@ function performRealtimeSearch() {
     let resultContainer = document.getElementById('dynamicResultContainer');
 
     if (!resultContainer) {
-        renderSearchResultPage(rawKeyword, type, false);
+        renderSearchResultPage(rawKeyword, type, true);
         return;
     }
 
@@ -208,25 +204,16 @@ function performRealtimeSearch() {
     }
 }
 
-// ========== 修改后的 renderSearchResultPage ==========
 function renderSearchResultPage(rawKeyword, type, restoreCursor = false) {
     if (!rawKeyword || rawKeyword.trim() === '') return;
 
-    // 标记当前在搜索结果页
-    currentView = 'searchResult';
-
-    // 生成滚动位置保存的 key
-    const searchResultKey = "searchResult_" + rawKeyword + "_" + type;
-
-    // 如果不是恢复模式（即首次进入），先保存当前页面的滚动位置
-    if (!restoreCursor) {
-        if (currentView === 'categories') {
-            saveScroll("categories");
-        } else if (currentView === 'seriesList' && currentCategoryId) {
-            saveScroll("seriesList_" + currentCategoryId);
-        } else if (currentView === 'copyList' && currentSeries) {
-            saveScroll("copyList_" + currentSeries.cid + "_" + currentSeries.si);
-        }
+    // 进入搜索结果页前，保存当前页面的滚动位置
+    if (currentView === 'categories') {
+        saveScroll("categories");
+    } else if (currentView === 'seriesList' && currentCategoryId) {
+        saveScroll("seriesList_" + currentCategoryId);
+    } else if (currentView === 'copyList' && currentSeries) {
+        saveScroll("copyList_" + currentSeries.cid + "_" + currentSeries.si);
     }
 
     const results = performSearch(rawKeyword, type, searchScope);
@@ -265,15 +252,17 @@ function renderSearchResultPage(rawKeyword, type, restoreCursor = false) {
 
     document.getElementById("app").innerHTML = fullHtml;
     
+    // 搜索结果页总是从顶部开始
+    window.scrollTo(0, 0);
+    
     bindSearchEvents();
 
     if (restoreCursor) {
-        // 从详情页返回：恢复滚动位置，但不恢复光标
-        restoreScroll(searchResultKey);
-    } else {
-        // 首次进入搜索结果页：滚动到顶部并保存滚动位置
-        window.scrollTo(0, 0);
-        saveScroll(searchResultKey);
+        const input = document.getElementById('searchInput');
+        if (input) {
+            input.focus();
+            input.setSelectionRange(input.value.length, input.value.length);
+        }
     }
 }
 
@@ -431,7 +420,6 @@ function bindSearchEvents() {
 }
 
 function renderCategories(restore = false) {
-    fromSearchResult = false;
     if (!restore) {
         window.scrollTo(0, 0);
     }
@@ -485,7 +473,6 @@ function renderCategories(restore = false) {
 }
 
 function renderSeriesList(cid, restore = false) {
-    fromSearchResult = false;
     if (!restore) {
         window.scrollTo(0, 0);
     }
@@ -541,7 +528,6 @@ function renderSeriesList(cid, restore = false) {
 }
 
 function renderCopyList(cid, si, restore = false) {
-    fromSearchResult = false;
     if (!restore) {
         window.scrollTo(0, 0);
     }
@@ -663,15 +649,8 @@ function renderDetail(cid, si, ci) {
     window.scrollTo(0, 0);
 }
 
-// ========== 修改后的 backToCopyList ==========
 function backToCopyList(cid, si) {
-    if (fromSearchResult) {
-        fromSearchResult = false;
-        // 传入 true 表示需要恢复滚动位置
-        renderSearchResultPage(lastSearchParams.keyword, lastSearchParams.type, true);
-    } else {
-        renderCopyList(cid, si, true);
-    }
+    renderCopyList(cid, si, true);
 }
 
 function backToSeries(cid) {
@@ -694,17 +673,7 @@ function selectSeries(cid, si) {
 }
 
 function selectCopy(cid, si, ci) {
-    // 判断当前是否在搜索结果页
-    if (currentView === 'searchResult') {
-        fromSearchResult = true;
-        lastSearchParams = {
-            keyword: currentSearchKeyword,
-            type: currentSearchType
-        };
-    } else {
-        fromSearchResult = false;
-        saveScroll("copyList_" + cid + "_" + si);
-    }
+    saveScroll("copyList_" + cid + "_" + si);
     renderDetail(cid, si, ci);
 }
 
